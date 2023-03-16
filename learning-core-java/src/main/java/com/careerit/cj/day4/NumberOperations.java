a8edd3d3f9162e3be9ad28301c86ddbcffcbcbae
javawithlwl <javawithlwl@gmail.com>
2023-03-16 06:17:08 +0530

    Added assignment questions impl

--- learning-core-java/src/main/java/com/careerit/cj/day4/NumberOperations.java
+++ learning-core-java/src/main/java/com/careerit/cj/day4/NumberOperations.java
@@ -3,11 +3,27 @@ package com.careerit.cj.day4;
 public class NumberOperations {
 
   public static void main(String[] args) {
+    int num = 98789;
+    int revNum = reverse(num);
+    System.out.println(num + " reverse num is :" + revNum);
+    if(isPalindrome(num)){
+      System.out.println(num +" is palindrome");
+    }else{
+      System.out.println(num+" is not a palindrome");
+    }
+
 
   }
 
   private static int reverse(int num) {
-    return 0;
+    int rev = 0;
+    while (num != 0) {
+      int r = num % 10;
+      rev = rev * 10 + r;
+      ;
+      num = num / 10;
+    }
+    return rev;
   }
 
   private static int sumOfDigits(int num) {
@@ -15,7 +31,7 @@ public class NumberOperations {
   }
 
   private static boolean isPalindrome(int num) {
-    return false;
+      return reverse(num) == num;
   }
 
   private static boolean isArmstrong(int num) {
